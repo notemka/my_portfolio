739f697c5e7b3038077979f114d70eef14ccefba
Emma Khayrullina <emma.saitgaraeva@gmail.com>
2015-07-28 23:31:25 +0400

    first files

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,112 @@
-﻿'use strict';
+﻿"use strict" //показывать место ошибки в терминале
 
-var gulp = require('gulp'),
+var gulp = require('gulp'),							//создание переменных для вызова
 	wiredep = require('wiredep').stream,
-	useref = require('gulp-useref'),
-	//sass = require('gulp-sass'),
+	useref = require('gulp-useref'),				//склеивает и рассовывает все по папкам, в html надо поставить специальные комментарии для css и js
 	uglify = require('gulp-uglify'),
 	clean = require('gulp-clean'),
 	gulpif = require('gulp-if'),
 	filter = require('gulp-filter'),
 	size = require('gulp-size'),
 	imagemin = require('gulp-imagemin'),
-	concatCss = require('gulp-concat-css'),
-	minifyCss = require('gulp-minify-css'),
+	minifyCss = require('gulp-minify-css'), 		//минификация файла css
 	jade = require('gulp-jade'),
 	prettify = require('gulp-prettify'),
 	browserSync = require('browser-sync'),
-	gutil = require('gulp-util'),
-	ftp = require('vinyl-ftp'),
 	reload = browserSync.reload;
-	
-// Подключем ссылки на bower components
-//gulp.task('wiredep', function(){
-//	gulp.src('app/*.html')
-//	.pipe(wiredep())
-//	.pipe(gulp.dest('app/'))
-//});
 
-// Компилируем jade в html
-gulp.task('jade', function(){
-	gulp.src('app/templates/pages/*.jade')
-	.pipe(jade())							// компиляция
-	.pipe('error', log)						// вывод ошибок
-	.pipe(prettify({indent_size: 2}))		// делаем красивые отступы
-	.pipe(gulp.dest('app/'))				// сохраняем в папку dest
-	.pipe(reload({stream: true}));			// перезагружаем страницу browserSync-ом
+///////////
+//сборка //
+///////////
+
+//очистка папки
+gulp.task('clean', function () {
+	return gulp.src('dist')
+		.pipe(clean());
 });
 
-// Подключем ссылки на bower components (переписываем wiredep под работу с jade-ом)
-gulp.task('wiredep', function () {
-	gulp.src('app/templates/common/*.jade')
-		.pipe(wiredep({
-			ignorePath: /^(\.\.\/)*\.\./
+//переносим html, css, js в папку dist
+gulp.task('useref', function () {
+	var assets = useref.assets();
+	return gulp.src('app/*.html')
+		.pipe(assets)
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.css', minifyCss({compability: 'ie8'})))
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest('dist')); //прописываем новые пути
+});
+
+//перенос шрифтов
+gulp.task('fonts', function(){
+	gulp.src('app/fonts/*')
+		.pipe(filter(['*.eot', '*.svg', '*.ttf', '*.woff', '*.woff2']))
+		.pipe(gulp.dest('dist/fonts/'));
+});
+
+//картинки
+gulp.task('images', function () {
+	return gulp.src('app/img/**/*')
+		.pipe(imagemin({
+			progressive: true,
+			interlaced:true
 		}))
-		.pipe(gulp.dest('app/templates/common/'))
+		.pipe(gulp.dest('dist/img'));
 });
 
-// слежка и запуск
-gulp.task('watch', function(){
-	gulp.watch('app/templates/**/*.jade');
-	gulp.watch('bower.json', ['wiredep']);		// если добавляется новая зависимость,то проходит сначала через wiredep
-	gulp.watch([
-		'app/js/**/*.js',
-		'app/css/**/*.css'
-	]).on('change', reload);
-	
+
+//остальные файлы, такие как favicon  и пр.
+gulp.task('extras', function () {
+	return gulp.src([
+		'app/*.*',
+		'!app/*.html'
+	]).pipe(gulp.dest('dist'));
 });
 
-// задача по умолчанию
-gulp.task('default', ['server', 'watch']);
+//сборка и вывод размера содержимого папки dist
+gulp.task('dist', ['useref', 'images', 'fonts', 'extras'], function(){
+	return gulp.src('dist/**/*').pipe(size({title:'build'}));
+});
+
+
+/////////////////
+//Работа с APP //
+/////////////////
+
+// Собираем папку DIST (только после компиляции Jade)
+gulp.task('build', ['clean', 'jade'], function () {
+	gulp.start('dist');
+});
+
+// Компилируем Jade в html
+gulp.task('jade', function() {
+	gulp.src('app/templates/pages/*.jade')
+		.pipe(jade())
+		.on('error', log)
+		.pipe(prettify({indent_size: 2}))
+		.pipe(gulp.dest('app/'))
+		.pipe(reload({stream: true}));
+});
+
+// Подключаем ссылки на bower components
+gulp.task('wiredep', function () {
+	gulp.src('app/*.html')
+		.pipe(wiredep())
+		.pipe(gulp.dest('app/'))
+});
+
+// bower components если пользоваться jade
+/*	gulp.task('wiredep', function () {
+ gulp.src('app/templates/common/*.jade')
+ .pipe(wiredep({
+ ignorePath: /^(\.\.\/)*\.\./
+ }))
+ .pipe(gulp.dest('app/templates/common/'))
+ });*/
 
 // Запускаем локальный сервер (только после компиляции jade)
-gulp.task('server', [jade], function(){
-	bowerSync({
+gulp.task('server', function () {
+	browserSync({
 		notify: false,
 		port: 9000,
 		server: {
@@ -70,62 +115,23 @@ gulp.task('server', [jade], function(){
 	});
 });
 
-// Очистка папки
-gulp.task('clean', function(){
-	return gulp.src('dist')
-	.pipe(clean())
+// слежка и запуск задач
+gulp.task('watch', function () {
+//	  gulp.watch('app/templates/**/*.jade', ['jade']);
+	gulp.watch('bower.json', ['wiredep']);
+	gulp.watch([
+		'app/*.html',
+		'app/js/**/*.js',
+		'app/css/**/*.css'
+	]).on('change', reload);
 });
 
-// Переносим HTML, CSS, JS в папку dist
- gulp.task('useref', function(){
-	var assets = useref.assets();
-	return gulp.src('app/*.html')
-	.pipe(assets)
-	.pipe(gulpif('*.js', uglify()))
-	.pipe(gulpif('*.css', minifyCss({compatibility: 'ie8'})))
-	.pipe(assets.restore())
-	.pipe(useref())
-	.pipe(gulp.dest('dist'));
-});
+// Задача по-умолчанию
+gulp.task('default', ['server', 'watch', 'wiredep']);
 
-// Перенос шрифтов
- gulp.task('fonts', function(){
-	gulp.src('app/fonts/*')
-	.pipe(filter(['*.eot', '*.svg', '*.ttf', '*.woff', '*.woff2']))
-	.pipe(gulp.dest('dist/fonts/'));
-});
-
-// Картинки
- gulp.task('images', function(){
-	return gulp.src('app/img/**/*')
-	.pipe(imagemin({
-		progressive: true,
-		interlaced: true
-	}))
-	.pipe(gulp.dest('dist/img'));
-});
-
-// Остальные файлы, такие как favicon.ico и пр.
- gulp.task('extras', function(){
-	return gulp.src([
-		'app/*.*',
-		'!app/*.html'
-	])
-	.pipe(gulp.dest('dist'));
-});
-
-// Сборка и вывод размера содержимого папки dist
- gulp.task('dist', ['useref', 'images', 'fonts', 'extras'], function (){
-		return gulp.src('dist/**/*').pipe(size({title: 'build'}));
-});
-
-// Собираем папку DIST
- gulp.task('bild', ['clean'], function (){
-		gulp.start('dist');
-});
-
-// Функции
-
+// ====================================================
+// ====================================================
+// ===================== Функции ======================
 
 // Более наглядный вывод ошибок
 var log = function (error) {
@@ -140,18 +146,10 @@ var log = function (error) {
 	this.end();
 }
 
-// ДЕПЛОЙ
-gulp.task('deploy', function(){
-	var conn = ftp.create({
-		host: '',
-		user: '',
-		password: '',
-		parallel: 10,
-		log: gutil.log
-	});
-	var globs = [
-		'dist/**/*'
-	];
-	return gulp.src(globs, {base: 'dist/', buffer: false})
-		.pipe(conn.dest('public_html/'))
-});
+
+// ====================================================
+// ====================================================
+// =============== Важные моменты  ====================
+// gulp.task(name, deps, fn)
+// deps - массив задач, которые будут выполнены ДО запуска задачи name
+// внимательно следите за порядком выполнения задач!
